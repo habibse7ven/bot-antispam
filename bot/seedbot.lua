ffd268bbaed11ba299b2b2330696d773a587804d
habibse7ven <habebezzi1382@gmail.com>
2016-03-16 09:51:34 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {97343715,114507663d,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -254,7 +254,7 @@ Persian: @IranSeed
     help_text_realm = [[
 Realm Commands:
 
-!creategroup [name]
+!Creategroup [name]
 Create a group
 
 !createrealm [name]
